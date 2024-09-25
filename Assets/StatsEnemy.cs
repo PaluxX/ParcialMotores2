6b2076db8eb66c7053870b98c325f055d17a0bf8
PaluxX <agus.palombo01@hotmail.com>
2024-09-25 00:44:56 -0300

    Nada

--- Assets/StatsEnemy.cs
+++ Assets/StatsEnemy.cs
@@ -6,4 +6,21 @@ public class StatsEnemy : MonoBehaviour
 {
     [SerializeField] private float _life = 100f;
     [SerializeField] private float _speed = 5f;
+
+
+
+
+
+
+
+
+    public void TakeDmg(float damage)
+    {
+        _life -= damage;
+    }
+    public void TakeDmG(float damage, Vector2 position)
+    {
+        _life -= damage;
+
+    }
 }
