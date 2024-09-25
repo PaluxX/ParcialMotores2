8e4f0a7541cf07e18c4bcfc86775cd0df6c72a83
PaluxX <agus.palombo01@hotmail.com>
2024-09-24 21:12:43 -0300

    Random

---
+++ Assets/StatsEnemy.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsEnemy : MonoBehaviour
+{
+    [SerializeField] private float _life = 100f;
+    [SerializeField] private float _speed = 5f;
+}
